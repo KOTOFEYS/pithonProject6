3bd25f5498065ea0b3d91821c7a4b1799af062b8
KOTOFEYS <makarov.k@mail.ru>
2024-08-26 00:05:27 +0300

    Дополнительное практическое задание по модулю*

--- modul5hard.py
+++ modul5hard.py
@@ -10,6 +10,7 @@ class UrTube:
     videos = []
     current_user = None
 
+
     def log_in(self, login: str, password: str):
         for user in self.users:
             if login == user.nickname and password == user.password:
@@ -41,17 +42,20 @@ class UrTube:
         return list_video
 
     def watch_video(self, video_clip: str):
-        if self.current_user and self.current_user.age < 18:
-            print('Вам нет 18 лет, пожалуйста покиньте страницу')
-        elif self.current_user:
-            for video in self.videos:
-                if video_clip in video.title:
-                    for i in range(1, 11):
-                        print(i, end=' ')
-                        time.sleep(1)
-                    print('Конец видео')
-        else:
-            print('Войдите в аккаунт, чтобы смотреть видео')
+        for video in self.videos:
+            if video.adult_mode:
+                self.adult_mode = True
+                if self.current_user and self.current_user.age < 18:
+                    print('Вам нет 18 лет, пожалуйста покиньте страницу')
+                elif self.current_user:
+                    for video in self.videos:
+                        if video_clip in video.title:
+                            for i in range(1, 11):
+                                print(i, end=' ')
+                                time.sleep(1)
+                                print('Конец видео')
+                else:
+                    print('Войдите в аккаунт, чтобы смотреть видео')
 
     def __str__(self):
         return f"{self.videos}"
@@ -93,7 +97,7 @@ class User:
 if __name__ == '__main__':
     ur = UrTube()
     v1 = Video('Лучший язык программирования 2024 года', 200)
-    v2 = Video('Для чего девушкам парень программист?', 10, adult_mode=True)
+    v2 = Video('Для чего девушкам парень программист?', 10, adult_mode = True)
 
     # Добавление видео
     ur.add(v1, v2)
